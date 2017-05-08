404a1183b09694c4b81c8e63156228af723bf310
--Angel Cabrera <soyangelcabrera@gmail.com>
2017-05-08 08:15:45 -0400

    Agregamos controladores y modficamos las rutas y vistas

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -79,20 +79,8 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                Laratter, by <a href="https://platzi.com">Platzi</a>
-            </div>
-
-            @if (isset($teacher))
-            <p>Profesor: {{ $teacher }}</p>
-            @else
-            <p>Profesor a definir</p>
-            @endif
-
-            <div class="links">
-                @foreach ($links as $link => $text)
-                <a href="{{ $link }}">{{ $text }}</a>
-                @endforeach
-            </div>
+                  Acerca de nostros!
+                </div>
             </div>
         </div>
     </body>
